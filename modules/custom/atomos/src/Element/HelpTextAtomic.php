0a8cefd461892c0bb1249380ee6d6ad73ab86aa5
Jáder Mecino Perea <jadermecino@gmail.com>
2019-03-20 07:54:04 -0500

    Cambio de posición twig y preprocess para el texto de ayuda

--- modules/custom/atomos/src/Element/HelpTextAtomic.php
+++ modules/custom/atomos/src/Element/HelpTextAtomic.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\atomos\Element;
 
+use Drupal\Core\Render\Element;
 use Drupal\Core\Render\Element\RenderElement;
 
 /**
@@ -15,9 +16,29 @@ class HelpTextAtomic extends RenderElement {
    * {@inheritdoc}
    */
   public function getInfo() {
+    $class = get_class($this);
     return [
+      '#pre_render' => [
+        [$class, 'preRenderHelpText'],
+      ],
       '#theme' => 'help_text_atomic',
     ];
+  }
+
+  /**
+   * Prepares a #type 'help_text_atomic' render element for help-text-atomic.html.twig.
+   *
+   * @param array $element
+   *   An associative array containing the properties of the element.
+   *   Properties used: #title and #attributes.
+   *
+   * @return array
+   *   The $element with prepared variables ready for help-text-atomic.html.twig.
+   */
+  public static function preRenderHelpText($element) {
+    Element::setAttributes($element, ['id', 'name']);
+    static::setAttributes($element, ['help-text-atomic']);
+    return $element;
   }
 
 }
